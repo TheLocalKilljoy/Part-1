891af3d24e978182439d6e3eeddf5b6b7918b73e
rosschas <rosschas@sheridancollege.ca>
2024-01-31 18:30:41 -0500

    Added player and player movement

---
+++ Assets/Assignment/Scripts/Move.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    float hz; float vt;
+
+    float speed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hz = Input.GetAxis("Horizontal");
+        transform.Translate(hz * speed * Time.deltaTime, 0, 0);
+
+        vt = Input.GetAxis("Vertical");
+        transform.Translate(0, vt * speed * Time.deltaTime, 0);
+    }
+}
